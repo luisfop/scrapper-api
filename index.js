58bec55533049b14978c2502502cc8a27b5c23da
luisop <luisfop77@gmail.com>
2021-10-25 11:16:04 -0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const PORT = 8000;
+
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
